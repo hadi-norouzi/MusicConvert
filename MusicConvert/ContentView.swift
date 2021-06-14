a8b1c6e4f7316ca9292976207f3b8bbe54eb10b5
hadinorouzi <hadinorouzi78@gmail.com>
2021-06-14 20:09:10 +0430

    Initial Commit

---
+++ MusicConvert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicConvert
+//
+//  Created by Hadi Norouzi on 6/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicConvert/MusicConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicConvertApp.swift
+//  MusicConvert
+//
+//  Created by Hadi Norouzi on 6/14/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
